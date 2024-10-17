14f1c8307a20f3549043861c571ac5bd14a98587
Tigo-cmd <ja16162040@gmail.com>
2024-10-18 00:46:52 +0100

    ..

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,7 @@
 -- An SQL script that creates a table of users with unique contents
 CREATE TABLE IF NOT EXISTS users (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  email VARCHAR(255) NOT NULL UNIQUE,
-  name VARCHAR(255),
-  country CHAR(2) ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country CHAR(2) ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
