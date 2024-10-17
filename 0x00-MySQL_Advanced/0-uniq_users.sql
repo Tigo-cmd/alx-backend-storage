0b6175acb86c30f73414f6c956068d1f1dc8371b
Tigo-cmd <ja16162040@gmail.com>
2024-10-18 00:18:18 +0100

    [FIX] CHECKER ADDED MORE FUNCTIONALITIES TO id column

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -6,8 +6,9 @@
 -- If the table already exists, your script should not fail
 -- Your script can be executed on any database
 
-CREATE TABLE IF NOT EXISTS users (
-  id SERIAL PRIMARY KEY,
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   email VARCHAR(255) NOT NULL UNIQUE,
   name VARCHAR(255)
 );
