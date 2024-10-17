0ffed72be4aed839efed5823d46c278e5da9dd2c
Tigo-cmd <ja16162040@gmail.com>
2024-10-18 00:25:48 +0100

    [Fix] Edite comments

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,4 @@
----  a SQL script that creates a table users following these requirements
-DROP TABLE IF EXISTS users;
+---  a SQL script that creates a table with unique users
 CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
