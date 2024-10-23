72a147fec62df24c6c12efed040d3e6de12bacb5
Tigo-cmd <ja16162040@gmail.com>
2024-10-23 20:48:57 +0100

    [Add] redis implementations

--- 0x01-NoSql/8-all.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-""" this module implements mongobd searching"""
-
-
-def list_all(mongo_collection):
-  """a Python function that lists all documents in a collection
-  """
-  return [item for item in mongo_collection.find()]
--- 0x01-NoSql/9-insert_school.py
+++
@@ -1,6 +0,0 @@
-all
-
-def insert_school(mongo_collection, **kwargs):
-    '''Inserts a new document in a collection.
-    '''
-    return mongo_collection.insert_one(kwargs).inserted_id
